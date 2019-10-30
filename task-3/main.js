e8e6a6488cb22c20f3f59a3285541d42c11430d2
Denys Semenov <54813799+DioCode@users.noreply.github.com>
2019-10-30 19:12:19 +0200

    Fix

--- task-3/main.js
+++ task-3/main.js
@@ -1,16 +1,16 @@
-let num1 = prompt("Type first number");
-let num2 = prompt("Type second number");
+let num1 = parseInt(prompt("Type first number"));
+let num2 = parseInt(prompt("Type second number"));
 let operation = prompt("Type operation");
 let result;
 
 if (operation == "+") {
-  result = Number.parseInt(num1) + Number.parseInt(num2);
+  result = num1 + num2;
 } else if (operation == "/") {
-  result = Number.parseInt(num1) / Number.parseInt(num2);
+  result = num1 / num2;
 } else if (operation == "*") {
-  result = Number.parseInt(num1) * Number.parseInt(num2);
+  result = num1 * num2;
 } else if (operation == "-") {
-  result = Number.parseInt(num1) - Number.parseInt(num2);
+  result = num1 - num2;
 }
 
 console.log("Your result is: " + result);
